693210042ae83159b2f6dee722c523049aa8a875
Ruslan Barna <ruslbarna@gmail.com>
2023-01-02 17:47:47 +0200

    header + navbar

--- src/App.js
+++ src/App.js
@@ -6,40 +6,15 @@ import linkedIn from "./img/icons/linkedIn.svg"
 import gitHub from "./img/icons/gitHub.svg"
 import instagram from "./img/icons/instagram.svg"
 
+import Navbar from "./components/navbar/Navbar"
+import Header from "./components/header/Header"
+
 function App() {
   return (
     <div className="App">
-    <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
+    <Navbar />
+	 <Header />
 
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                </button>
-
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Ruslan</em></strong><br/>
-                a frontend developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
-        </div>
-    </header>
 
     <main className="section">
         <div className="container">
