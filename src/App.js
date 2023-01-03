172c491b2f23a4b4561680567a9cfab699cfc24c
Ruslan Barna <ruslbarna@gmail.com>
2023-01-03 12:02:51 +0200

    pages

--- src/App.js
+++ src/App.js
@@ -2,51 +2,21 @@ import "./styles/main.css"
 import sun from "./img/icons/sun.svg"
 import moon from "./img/icons/moon.svg"
 
-import linkedIn from "./img/icons/linkedIn.svg"
-import gitHub from "./img/icons/gitHub.svg"
-import instagram from "./img/icons/instagram.svg"
-
 import Navbar from "./components/navbar/Navbar"
 import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Home from "./pages/Home"
+import Projects from "./pages/Project"
+import Contacts from "./pages/Contacts"
 
 function App() {
   return (
     <div className="App">
     <Navbar />
-	 <Header />
-
-
-    <main className="section">
-        <div className="container">
-
-                <ul className="content-list">
-                    <li className="content-list__item">
-                        <h2 className="title-2">Frontend</h2>
-                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-                    </li>
-                    <li className="content-list__item">
-                        <h2 className="title-2">Backend</h2>
-                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-                    </li>
-                </ul>
-
-        </div>
-    </main>
-
-    <footer className="footer">
-        <div className="container">
-            <div className="footer__wrapper">
-                <ul className="social">   
-                    <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
-                </ul>
-                <div className="copyright">
-                    <p>© 2022 frontend-dev.com</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+	 <Home />
+	 <Projects />
+	 <Contacts />
+    <Footer />
     </div>
   );
 }
