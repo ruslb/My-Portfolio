c3236b8e26625193892e4d8e121f75f0bfca7a84
Ruslan Barna <ruslbarna@gmail.com>
2023-01-04 12:04:28 +0200

    props

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,6 @@
 
-import Project from '../components/project/Project'
-import project02 from './../img/projects/02.jpg'
-import project03 from './../img/projects/03.jpg'
-import project04 from './../img/projects/04.jpg'
-import project05 from './../img/projects/05.jpg'
-import project06 from './../img/projects/06.jpg'
+import Project from '../components/project/Project';
+import {projects} from "./../helpers/projectsList"
 
 
 const Projects = () => {
@@ -13,32 +9,9 @@ const Projects = () => {
 		<div className="container">
 			 <h2 className="title-1">Projects</h2>
 			 <ul className="projects">
-	     	<Project />
-				  <li className="project">
-						<a href="./project-page.html">
-							 <img src={project02} alt="Project img" className="project__img" />
-							 <h3 className="project__title">Video service</h3>
-						</a>
-				  </li>
-				  <li className="project">
-						<a href="./project-page.html">
-							 <img src={project03} alt="Project img" className="project__img" />
-							 <h3 className="project__title">Video portal</h3>
-						</a>
-				  </li>
-
-				  <li className="project">
-						<img src={project04} alt="Project img" className="project__img" />
-						<h3 className="project__title">Dating app</h3>
-				  </li>
-				  <li className="project">
-						<img src={project05} alt="Project img" className="project__img" />
-						<h3 className="project__title">Landing</h3>
-				  </li>
-				  <li className="project">
-						<img src={project06} alt="Project img" className="project__img" />
-						<h3 className="project__title">Gaming community</h3>
-				  </li>
+	       {projects.map((project) => {
+				return <Project title={project.title} img={project.img} />;
+			 })}
 
 			 </ul>
 		</div>
